b76c93292e9fda999f036819d41ba3c2e1394bc6
Jong <jm991014@gmail.com>
2025-04-03 18:19:31 +0900

    [1주차] 백준_silver_차집합_김종명

---
+++ week1/백준_silver_차집합.kt
@@ -0,0 +1,13 @@
+package week1
+
+private val br = System.`in`.bufferedReader()
+
+fun main() = with(System.out.bufferedWriter()) {
+    val (n, m) = br.readLine().split(" ").map { it.toInt() }
+    val A = br.readLine().split(" ").map { it.toInt() }.toSortedSet()
+    val B = br.readLine().split(" ").map { it.toInt() }.toSortedSet()
+    val answer = A - B
+
+    write("${answer.size}\n${answer.joinToString(" ")}")
+    close()
+}
