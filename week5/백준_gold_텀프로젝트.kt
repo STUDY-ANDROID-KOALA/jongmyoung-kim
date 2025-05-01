77fd20f5d3b3d965ce47b2963259bdb9ed750cae
Jong <jm991014@gmail.com>
2025-05-01 17:42:45 +0900

    [5주차] 백준_gold_텀프로젝트_김종명

---
+++ week5/백준_gold_텀프로젝트.kt
@@ -0,0 +1,38 @@
+package week5
+
+private val br = System.`in`.bufferedReader()
+private var count = 0
+
+fun main() = with(System.out.bufferedWriter()) {
+    repeat(br.readLine().toInt()) {
+        val n = br.readLine().toInt()
+        val students = br.readLine().split(" ").map { it.toInt() - 1 }.toIntArray()
+        val finished = BooleanArray(n)
+        val visited = BooleanArray(n)
+
+        for (i in 0 until n) {
+            if (!visited[i]) students.dfs(i, finished, visited)
+        }
+
+        write("${n - count}\n")
+        count = 0
+    }
+    close()
+}
+
+private fun IntArray.dfs(student: Int, finished: BooleanArray, visited: BooleanArray) {
+    visited[student] = true
+    val next = this[student]
+
+    if (!visited[next]) dfs(next, finished, visited)
+
+    if (!finished[next]) {
+        var tmp = next
+        while (tmp != student) {
+            count++
+            tmp = this[tmp]
+        }
+        count++
+    }
+    finished[student] = true
+}
