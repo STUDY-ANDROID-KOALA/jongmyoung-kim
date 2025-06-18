13b3c11ce93eb897b0a126440a07b237f45797fb
Jong <jm991014@gmail.com>
2025-06-19 00:52:40 +0900

    [12주차] 코틀린 하나 자바 하나

---
+++ week12/SWEA_D3_방울마술.java
@@ -0,0 +1,48 @@
+package week12;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class SWEA_D3_방울마술 {
+    private static int bell;
+    private static int mix;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int tc =  Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= tc; i++) {
+            String[] input = br.readLine().split(" ");
+            bell = input[0].indexOf('o');
+            mix = Integer.parseInt(input[1]);
+
+            if (mix < 1) bw.write("#" + i + " " + bell + "\n");
+            else {
+                mixCup();
+                bw.write("#" + i + " " + bell + "\n");
+            }
+        }
+        bw.close();
+    }
+
+    private static void mixCup() {
+        while(mix > 0) {
+            switch (bell) {
+                case 0:
+                    bell = 1;
+                    break;
+                case 1:
+                    bell = 0;
+                    break;
+                case 2:
+                    bell = 1;
+                    break;
+            }
+            mix--;
+        }
+    }
+}
---
+++ week12/백준_silver_덩치.kt
@@ -0,0 +1,17 @@
+package week12
+
+private val br =  System.`in`.bufferedReader()
+
+fun main() = with(System.out.bufferedWriter()) {
+    val N = br.readLine().toInt()
+    val body = Array(N) { br.readLine().split(" ").map { it.toInt() }.let { it[0] to it[1] } }
+
+    body.forEach { (weightA, heightA) ->
+        var rank = 1
+        body.forEach { (weightB, heightB) ->
+            if (heightB > heightA && weightB > weightA) rank++
+        }
+        write("$rank ")
+    }
+    close()
+}
