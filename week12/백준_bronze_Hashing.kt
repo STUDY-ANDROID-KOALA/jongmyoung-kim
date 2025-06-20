5e0bbccef0f6c376226f1322ae6f4a9a24f65228
Jong <jm991014@gmail.com>
2025-06-21 00:50:42 +0900

    [12주차] 코틀린 둘

---
+++ week12/백준_bronze_Hashing.kt
@@ -0,0 +1,20 @@
+package week12
+
+import java.math.BigInteger
+
+private val br = System.`in`.bufferedReader()
+private val base = BigInteger.valueOf(31)
+private val mod =  BigInteger.valueOf(1234567891)
+private var code = BigInteger.ZERO
+
+fun main() = with(System.out.bufferedWriter()) {
+    val L = br.readLine().toInt()
+    val s = br.readLine()
+
+    s.forEachIndexed { index, alphabet ->
+        val power = base.pow(index)
+        code += BigInteger.valueOf((alphabet.code - 96).toLong()).multiply(power)
+    }
+    write("${code % mod}")
+    close()
+}
